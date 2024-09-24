8062bdec2b98fedfd6996fdebd67995e39ce974b
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-09-24 19:22:53 +0800

    Added Celsius to Fahrenheit and Fahrenheit to Celsius

--- temperature_converter.py
+++ temperature_converter.py
@@ -1,4 +1,4 @@
-# pseudocode
+# Temperature Converter
 
 #  Ask the user to input a temperature
 temperature = float(input(f"Temperature Converter --- \n"
@@ -9,4 +9,13 @@ conversion_type = int(input(f"Conversion Type --- \n"
                             f"2. Fahrenheit to Celsius \n"))
 
 #  Computation for Celsius to Fahrenheit
+if conversion_type == 1:
+    celsius_to_fahrenheit = (temperature * 9/5) + 32
+    print(f"Temperature in Celsius: {temperature}C°\n")
+    print(f"Temperature to Fahrenheit: {celsius_to_fahrenheit}F°")
+
 #  Computation for Fahrenheit to Celsius
+if conversion_type == 2:
+    celsius_to_fahrenheit = (temperature - 32) * 5/9
+    print(f"Temperature in Fahrenheit: {temperature}F°\n")
+    print(f"Temperature to Celsius: {celsius_to_fahrenheit}C°")
