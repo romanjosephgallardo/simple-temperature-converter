b2bf871263830f8d1fe9de8d45307a0b3efa79fd
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-09-24 19:13:58 +0800

    Prompt user to input temperature and conversion type.

--- temperature_converter.py
+++ temperature_converter.py
@@ -1,6 +1,12 @@
 # pseudocode
 
 #  Ask the user to input a temperature
+temperature = float(input(f"Temperature Converter --- \n"
+                          f"Enter temperature: "))
 #  Ask user to select conversion type
+conversion_type = int(input(f"Conversion Type --- \n"
+                            f"1. Celsius to Fahrenheit \n"
+                            f"2. Fahrenheit to Celsius \n"))
+
 #  Computation for Celsius to Fahrenheit
 #  Computation for Fahrenheit to Celsius
